bfbfc3bfd2404a367be80a21aa9f77d12a39750f
SrinivaasGudur07 <srinivaasguduru@gmail.com>
2026-03-25 01:12:12 +0530

    added episode-6

---
+++ Episode-6/src/components/Header.js
@@ -0,0 +1,38 @@
+import {  LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
+
+
+const Header = ()=>{
+
+    const[btnNameReact,setBtnNameReact]=useState("Login");
+
+    return (
+        <div className="header">
+
+            <div className="logo-container">
+                <img 
+                    className="logo"
+                    src={LOGO_URL}
+                />
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button 
+                        className="login"
+                        onClick={()=>{
+                            setBtnNameReact("Logout")
+                        }}>{btnNameReact}</button>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
